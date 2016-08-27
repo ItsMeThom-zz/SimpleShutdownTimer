e4fb0b09166a954436d5a234f039f4894e476359
Kerbobotat <thom.lambert@outlook.com>
2016-08-27 22:19:10 +0100

    Implemented Countdown timer in KillSwitch Set it to start on button click

---
+++ SimpleShutdownTimer/KillSwitch.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleShutdownTimer
+{
+    class KillSwitch
+    {
+        //this object holds the time (in secononds) until the program initates the killswitch
+        private TimeSpan countdown;
+        private TimeSpan One = new TimeSpan(0, 0, 1); //1 Second
+        private TimeSpan ZeroHour = new TimeSpan(0, 0, 0); //End Time
+
+        public TimeSpan Countdown {
+            get
+            {
+                return countdown;
+            }
+            set
+            {
+                countdown = value;
+            }
+
+        }
+
+        public bool Tick()
+        {
+            try
+            {
+                countdown = countdown.Subtract(One);
+            }
+            catch(Exception e)
+            {
+                var dialog = new Windows.UI.Popups.MessageDialog(e.Message);
+                var res = dialog.ShowAsync();
+            }
+
+            if (countdown <= ZeroHour)
+            {
+                return true; //Countdown is completed, return true
+            }
+            else return false;
+
+
+        }
+
+        public void Engage()
+        {
+            //pipe shutdown command to console here
+            
+        }
+
+
+        }
+
+    }
+
